f9ed856f783b0e19f6e7784547a9953b97216261
ohuohuo <zczou@hotmail.com>
2017-05-10 22:03:48 -0400

    added polls module

---
+++ polls/admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from polls.models import Question
+
+# Register your models here.
+admin.site.register(Question)
